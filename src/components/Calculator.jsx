75c02f1e0ffabe747e5a9b41d147cbd502fd3d6d
Ashwini Patil <ashwini.aski@gmail.com>
2023-09-27 11:14:56 -0400

    Initial commits

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+function Calculator() {
+
+    return (
+        /*Screen */
+        <div className="bg-gray-800 text-white w-screen h-screen flex justify-center items-center">
+            {/* <div className="">
+                <div className="screen p-2">
+
+                </div>
+
+
+            </div> */}
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+
+
+
+    )
+}
+
+export default Calculator;
